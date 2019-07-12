bc0899e4c756a9990a6cb618a2c8ad05284ef9ec
pramchou <52236418+pramchou@users.noreply.github.com>
2019-07-12 14:06:06 +0200

    Update HelloWorld.bat

--- HelloWorld.bat
+++ HelloWorld.bat
@@ -1,6 +1,4 @@
-git.exe 'https://github.com/pramchou/TestToolLib.git'
 git.exe remote set-head origin -a
-git.exe branch -a
 git.exe checkout -b NewBranch
-git.exe push origin NewBranch
+git.exe branch -a
 echo "Running after commit"
