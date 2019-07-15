f1d4efacbe8b7d95f8d48e47b4a738d27c1a97a4
pramchou <pramodkumar.chougule@capgemini.com>
2019-07-15 14:00:18 +0200

    HaloWorls modified

--- HelloWorld.bat
+++ HelloWorld.bat
@@ -1,5 +1,7 @@
 git remote set-head origin -a
-git checkout -b NewBranch
+git checkout -b NewBranch9
 git branch -a
-git push origin NewBranch
+git status
+git pull
+git push origin NewBranch9
 echo "Running after commit"
